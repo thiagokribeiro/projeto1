d4301dbca71a8cc9ee75c9b15c49a4ab6c3d11c8
thiagokribeiro <thiagoribeiro92@gmail.com>
2025-03-25 18:09:51 -0300

    transaction repository

---
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -0,0 +1,35 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getUserType() == UserType.MERCHANT){
+            throw new Exception("Usuário do tipo lojista não está autorizado a realizar a transação");
+        }
+
+        if(sender.getBalance().compareTo(amount)<0){
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return  this.userRepository.findUserById(id).orElseThrow(() -> new Exception("usuario nao encontrado"));
+
+    }
+
+    public void saveUser(User user){
+        this.userRepository.save(user);
+    }
+}
